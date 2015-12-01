c33e69b26ac0c6c76c5986bda25cb26250545919
CarsonWu <carsonwu@cherrypicks.com>
2015-12-01 15:03:46 +0800

    UI added, basic logic added

--- OTA Builder/AppDelegate.h
+++ OTA Builder/AppDelegate.h
@@ -10,6 +10,21 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (weak) IBOutlet NSButton *buildButton;
+@property (weak) IBOutlet NSPathControl *projectPath;
+@property (weak) IBOutlet NSPathControl *repoPath;
+@property (weak) IBOutlet NSProgressIndicator *spinner;
+@property (weak) IBOutlet NSTextField *targetName;
+@property (unsafe_unretained) IBOutlet NSTextView *outputText;
+- (IBAction)startTask:(id)sender;
+- (IBAction)stopTask:(id)sender;
+
+/**
+ * NSTask
+ */
+@property (nonatomic, strong) __block NSTask *buildTask;
+@property (nonatomic) BOOL isRunning;
+@property (nonatomic, strong) NSPipe *outputPipe;
 
 @end
 
--- OTA Builder/AppDelegate.m
+++ OTA Builder/AppDelegate.m
@@ -23,4 +23,62 @@
     // Insert code here to tear down your application
 }
 
+- (void)runScriptWithArguments:(NSArray*)arguments{
+    //1
+    dispatch_queue_t taskQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+    dispatch_async(taskQueue, ^{
+        //2
+        self.isRunning = YES;
+        
+        //3
+        @try {
+            // 01
+            NSString *path = [NSString stringWithFormat:@"%@", [[NSBundle mainBundle] pathForResource:@"BuildScript" ofType:@"command"]];
+            // 02
+            self.buildTask = [[NSTask alloc] init];
+            self.buildTask.launchPath = path;
+            self.buildTask.arguments = arguments;
+            [self.buildTask launch];
+            [self.buildTask waitUntilExit];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Problem runnig task:%@", [exception description]);
+        }
+        @finally {
+            [self.buildButton setEnabled:YES];
+            [self.spinner stopAnimation:self];
+            self.isRunning = NO;
+        }
+    });
+}
+
+#pragma mark - IBAction
+
+- (IBAction)startTask:(id)sender {
+    
+    self.outputText.string = @"";
+    
+    NSString *projectLocation = self.projectPath.URL.path;
+    NSString *finalLocation = self.repoPath.URL.path;
+    
+    NSString *projectName = self.projectPath.URL.lastPathComponent;
+    NSString *xcodeProjectFile = [projectLocation stringByAppendingString:[NSString stringWithFormat:@"/%@.xcodeproj", projectName]];
+    
+    NSString *buildLocation = projectLocation;//[projectLocation stringByAppendingString:@"/build/Release-iphoneos"];
+    
+    NSMutableArray *arguments = [[NSMutableArray alloc] init];
+    [arguments addObject:xcodeProjectFile];
+    [arguments addObject:self.targetName.stringValue];
+    [arguments addObject:buildLocation];
+    [arguments addObject:projectName];
+    [arguments addObject:finalLocation];
+    
+    [self.buildButton setEnabled:NO];
+    [self.spinner startAnimation:self];
+    [self runScriptWithArguments:arguments];
+}
+
+- (IBAction)stopTask:(id)sender {
+}
+
 @end
